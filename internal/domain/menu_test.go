572f0743c4ceacbd103b8a028863cd6d9e3dc31e
Suiren91 <rento8191@gmail.com>
2026-07-08 12:39:26 +0900

    Change: TestNewMenu() to table-driven

--- internal/domain/menu_test.go
+++ internal/domain/menu_test.go
@@ -1,37 +1,114 @@
 package domain_test
 
 import (
+	"errors"
 	"testing"
 
 	"github.com/Suiren91/go-cafeteria/internal/domain"
 )
 
-const (
-	ID          int    = 1
-	NAME        string = "かぼちゃ"
-	DESCRIPTION string = "ほくほくでうまい"
-	PRICE       int    = 150
-	STOCK       int    = 100
-)
-
 func TestNewMenu(t *testing.T) {
-	got, err := domain.NewMenu(ID, NAME, DESCRIPTION, PRICE, STOCK)
-	if err != nil {
-		t.Fatalf("NewMenu() returned unexpected error: %v", err)
+	tests := map[string]struct {
+		id          int
+		name        string
+		description string
+		price       int
+		stock       int
+		wantErr     error
+	}{
+		"適切なパラメータでメニューを作成できる": {
+			id:          1,
+			name:        "かぼちゃ",
+			description: "ほくほくでうまい",
+			price:       150,
+			stock:       100,
+			wantErr:     nil,
+		},
+		"名前が空文字だとエラー": {
+			id:          1,
+			name:        "",
+			description: "ほくほくでうまい",
+			price:       150,
+			stock:       100,
+			wantErr:     domain.ErrInvalidName,
+		},
+		"名前が空白のみだとエラー": {
+			id:          1,
+			name:        " ",
+			description: "ほくほくでうまい",
+			price:       150,
+			stock:       100,
+			wantErr:     domain.ErrInvalidName,
+		},
+		"説明が空文字でもメニューを作成できる": {
+			id:          1,
+			name:        "かぼちゃ",
+			description: "",
+			price:       150,
+			stock:       100,
+			wantErr:     nil,
+		},
+		"priceが0でもメニューを作成できる": {
+			id:          1,
+			name:        "かぼちゃ",
+			description: "ほくほくでうまい",
+			price:       0,
+			stock:       100,
+			wantErr:     nil,
+		},
+		"priceが負の数(-1)だとエラー": {
+			id:          1,
+			name:        "かぼちゃ",
+			description: "ほくほくでうまい",
+			price:       -1,
+			stock:       100,
+			wantErr:     domain.ErrNegativePrice,
+		},
+		"stockが0でもメニューを作成できる": {
+			id:          1,
+			name:        "かぼちゃ",
+			description: "ほくほくでうまい",
+			price:       150,
+			stock:       0,
+			wantErr:     nil,
+		},
+		"stockが負の数(-1)だとエラー": {
+			id:          1,
+			name:        "かぼちゃ",
+			description: "ほくほくでうまい",
+			price:       150,
+			stock:       -1,
+			wantErr:     domain.ErrNegativeStock,
+		},
 	}
-	if got.ID() != ID {
-		t.Errorf("ID want: %d, got: %d", ID, got.ID())
-	}
-	if got.Name() != NAME {
-		t.Errorf("Name want: %v, got: %v", NAME, got.Name())
-	}
-	if got.Description() != DESCRIPTION {
-		t.Errorf("Description want: %v, got: %v", DESCRIPTION, got.Description())
-	}
-	if got.Price() != PRICE {
-		t.Errorf("Price want: %d, got:%d", PRICE, got.Price())
-	}
-	if got.Stock() != STOCK {
-		t.Errorf("Stock want: %d, got: %d", STOCK, got.Stock())
+	for name, tt := range tests {
+		t.Run(name, func(t *testing.T) {
+			got, err := domain.NewMenu(tt.id, tt.name, tt.description, tt.price, tt.stock)
+
+			if !errors.Is(err, tt.wantErr) {
+				t.Fatalf("err want: %v, got: %v", tt.wantErr, err)
+			}
+			if tt.wantErr != nil {
+				if got != nil {
+					t.Errorf("Menu want: nil, got: %v", got)
+				}
+				return
+			}
+			if got.ID() != tt.id {
+				t.Errorf("ID want: %d, got: %d", tt.id, got.ID())
+			}
+			if got.Name() != tt.name {
+				t.Errorf("Name want: %v, got: %v", tt.name, got.Name())
+			}
+			if got.Description() != tt.description {
+				t.Errorf("Description want: %v, got: %v", tt.description, got.Description())
+			}
+			if got.Price() != tt.price {
+				t.Errorf("Price want: %d, got:%d", tt.price, got.Price())
+			}
+			if got.Stock() != tt.stock {
+				t.Errorf("Stock want: %d, got: %d", tt.stock, got.Stock())
+			}
+		})
 	}
 }
