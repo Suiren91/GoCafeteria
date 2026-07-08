e0f62741b74ffa83bca185e0b6d4714b8e3ca267
Suiren91 <rento8191@gmail.com>
2026-07-08 12:36:57 +0900

    Add: sentinel errors for Menu

---
+++ internal/domain/errors.go
@@ -0,0 +1,10 @@
+package domain
+
+import "errors"
+
+var (
+	ErrNegativePrice = errors.New("price must be non-negative")
+	ErrNegativeStock = errors.New("stock must be non-negative")
+	ErrInvalidID     = errors.New("id must be positive")
+	ErrInvalidName   = errors.New("name must not be empty")
+)
