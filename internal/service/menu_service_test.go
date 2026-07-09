087fd229bce7e0a642ce136b4a3ca2f8e8a3f383
Suiren91 <rento8191@gmail.com>
2026-07-09 12:24:03 +0900

    test(service): refactor tests to table-driven

--- internal/service/menu_service_test.go
+++ internal/service/menu_service_test.go
@@ -1,22 +1,44 @@
 package service_test
 
 import (
+	"errors"
 	"testing"
 
+	"github.com/Suiren91/go-cafeteria/internal/domain"
 	"github.com/Suiren91/go-cafeteria/internal/service"
 )
 
-const (
-	ID          int    = 1
-	NAME        string = "かぼちゃ"
-	DESCRIPTION string = "ほくほくでうまい"
-	PRICE       int    = 150
-	STOCK       int    = 100
-)
-
 func TestCreateNewMenu(t *testing.T) {
-	err := service.CreateNewMenu(ID, NAME, DESCRIPTION, PRICE, STOCK)
-	if err != nil {
-		t.Errorf("TestCreateNewMenu failed: %v", err)
+	tests := map[string]struct {
+		id          int
+		name        string
+		description string
+		price       int
+		stock       int
+		wantErr     error
+	}{
+		"適切なパラメータでメニューを新規作成できる": {
+			id:          1,
+			name:        "かぼちゃ",
+			description: "ほくほくでうまい",
+			price:       150,
+			stock:       100,
+			wantErr:     nil,
+		}, "priceが負の数(-1)だとエラー": {
+			id:          1,
+			name:        "かぼちゃ",
+			description: "ほくほくでうまい",
+			price:       -1,
+			stock:       100,
+			wantErr:     domain.ErrNegativePrice,
+		},
+	}
+	for name, tt := range tests {
+		t.Run(name, func(t *testing.T) {
+			err := service.CreateNewMenu(tt.id, tt.name, tt.description, tt.price, tt.stock)
+			if !errors.Is(err, tt.wantErr) {
+				t.Errorf("want: %v, got: %v", tt.wantErr, err)
+			}
+		})
 	}
 }
