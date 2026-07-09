83ae4e9d5112fa01ebf14237933c0c22b848a181
Suiren91 <rento8191@gmail.com>
2026-07-09 14:00:50 +0900

    fix(service): constructor argument

--- internal/service/menu_service.go
+++ internal/service/menu_service.go
@@ -2,9 +2,13 @@ package service
 
 import "github.com/Suiren91/go-cafeteria/internal/domain"
 
+// TODO: id管理の方法を模索
+var id int = 1
+
 // TODO: 構造体とコンストラクタ
-func CreateNewMenu(id int, name, description string, price, stock int) error {
+func CreateNewMenu(name, description string, price, stock int) error {
 	_, err := domain.NewMenu(id, name, description, price, stock)
+	id++
 	// TODO: DBへの保存処理
 	if err != nil {
 		// TODO: 適切なエラーに翻訳して返却
