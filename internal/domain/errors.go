86e03a3418071491b3145ba39d6a2b85d27a9709
Suiren91 <rento8191@gmail.com>
2026-09-04 21:42:10 +0900

    feat(domain): add sentinel error "ErrUserNotFound"

--- internal/domain/errors.go
+++ internal/domain/errors.go
@@ -9,4 +9,5 @@ var (
 	ErrInvalidName   = errors.New("name must not be empty")
 	ErrMenuNotFound  = errors.New("menu not found")
 	ErrInvalidEmail  = errors.New("invalid email address")
+	ErrUserNotFound  = errors.New("user not found")
 )
