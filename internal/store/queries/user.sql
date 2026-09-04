545be3523384bbb9717f574d8d0f51b2abbc5b12
Suiren91 <rento8191@gmail.com>
2026-09-04 20:49:44 +0900

    feat(store): add users table query and fix menus query - menus query fixes updated_at

---
+++ internal/store/queries/user.sql
@@ -0,0 +1,11 @@
+-- name: GetUser :one
+SELECT * FROM users WHERE id = $1;
+
+-- name: ListUsers :many
+SELECT * FROM users ORDER BY id;
+
+-- CreateUser :one
+INSERT INTO users(id, email, name) VALUES ($1,$2,$3) RETURNING *;
+
+-- UpdateUser :one
+UPDATE users SET email=$2, name=$3, updated_at=now() WHERE id=$1 RETURNING *;
